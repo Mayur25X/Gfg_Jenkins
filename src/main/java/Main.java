1c82579775067c15616a46c25da4254c799fb65e
Mayur25X <mayuruikey5678@gmail.com1>
2024-02-22 23:59:46 +0530

    Div Fun() added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,22 @@ public class Main {
 		System.out.println("Mul of Two number : " + a * b);
 	}
 
+	public void div(int a, int b) {
+
+		try {
+			float c = a / b;
+			System.out.println("Div of Two number : " + c);
+		} catch (Exception e) {
+			System.out.println("Not divible");
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 		Main a = new Main();
 		a.sum(10, 5);
 		a.mul(10, 5);
+		a.div(10, 5);
 	}
 }
